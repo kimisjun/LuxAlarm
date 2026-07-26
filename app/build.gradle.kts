ce3cbd9743c10f42f6ebcd9b3c216947209c73e1
Daniel Salmun <salmundani@gmail.com>
2026-07-25 22:53:01 -0300

    chore: release 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.dsalmun.luxalarm"
         minSdk = 28
         targetSdk = 37
-        versionCode = 11
-        versionName = "2.2.3"
+        versionCode = 12
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
